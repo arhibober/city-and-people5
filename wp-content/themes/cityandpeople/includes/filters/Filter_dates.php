833d645bb24c49be27e0ae453edaf2d1aae0ba01
arhibober <alex_lvovich_iff@mail.ru>
2022-06-17 13:16:37 +0300

    Date filter was edited

--- wp-content/themes/cityandpeople/includes/filters/Filter_dates.php
+++ wp-content/themes/cityandpeople/includes/filters/Filter_dates.php
@@ -8,8 +8,9 @@ class Filter_dates
 	{
 		// create $args['meta_query'] array if one of the following fields is filled
 		$args = [];
-		if (isset($_POST['new_date']) && $_POST['new_date'] || isset($_POST['old_date']) && $_POST['old_date'])
+		if (isset($_POST['new_date']) && $_POST['new_date'] || isset($_POST['old_date']) && $_POST['old_date']) {
 			$args['meta_query'] = array('relation' => 'AND'); // AND means that all conditions of meta_query should be true
+		}
 
 		// if both minimum price and maximum price are specified we will use BETWEEN comparison
 		if (isset($_POST['new_date']) && $_POST['new_date'] && isset($_POST['old_date']) && $_POST['old_date']) {
@@ -21,29 +22,34 @@ class Filter_dates
 			);
 		} else {
 			// if only min price is set
-			if (isset($_POST['old_date']) && $_POST['old_date'])
+			if (isset($_POST['old_date']) && $_POST['old_date']) {
 				$args['meta_query'][] = array(
 					'key' => 'дата',
 					'value' => $_POST['old_date'],
 					'type' => 'date',
 					'compare' => '>'
 				);
+			}
 
 			// if only max price is set
-			if (isset($_POST['new_date']) && $_POST['new_date'])
+			if (isset($_POST['new_date']) && $_POST['new_date']) {
 				$args['meta_query'][] = array(
 					'key' => 'дата',
 					'value' => $_POST['new_date'],
 					'type' => 'date',
 					'compare' => '<'
 				);
+			}
 		}
 		$args['post_type'] = "city_object";
 		$taxonomies = "";
-		if (count($_POST["taxonomies"]) > 0)
-			$args["tax_query"][0]["taxonomy"] = "city_object_taxonomy";
-		foreach ($_POST["taxonomies"] as $taxonomy)
-			$args["tax_query"][0]["terms"][] = $taxonomy;
+		if (isset($_POST['taxonomies'])) {
+			if (count($_POST['taxonomies']) > 0) {
+				$args['tax_query'][0]['taxonomy'] = 'city_object_taxonomy';
+				foreach ($_POST['taxonomies'] as $taxonomy)
+					$args['tax_query'][0]['terms'][] = $taxonomy;
+			}
+		}
 		$query = new WP_Query($args);
 		if ($query->have_posts()) {
 			while ($query->have_posts()) {
