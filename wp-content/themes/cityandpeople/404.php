1639cb3c64bcde467f92cab6a7a26d8013236566
arhibober <alex_lvovich_iff@mail.ru>
2022-07-11 13:04:01 +0300

    To 404 page the site column was added

--- wp-content/themes/cityandpeople/404.php
+++ wp-content/themes/cityandpeople/404.php
@@ -13,50 +13,51 @@ get_header('v2'); ?>
 <!-- Page Content -->
 <div class='container'>
 
-    <!-- Page Heading/Breadcrumbs -->
-    <h1 class='mt-4 mb-3'>404
-        <small>
-            <?php _e('Page Not Found') ?>
-        </small>
-    </h1>
+    <!-- Marketing Icons Section -->
+    <div class='row'>
+        <div class='col-md-8'>
+            <!-- Blog Entries Column -->
+            <div id="object_archive" class='row'>
 
-    <ol class='breadcrumb'>
-        <li class='breadcrumb-item'>
-            <a href='index.html'>Home</a>
-        </li>
-        <li class='breadcrumb-item active'>404</li>
-    </ol>
+                <!-- Page Heading/Breadcrumbs -->
+                <h1 class='mt-4 mb-3'>404
+                    <small>
+                        <?php _e('Page Not Found') ?>
+                    </small>
+                </h1>
 
-    <div class='jumbotron'>
-        <h1 class='display-1'>404</h1>
-        <p>
-            <?php _e("The page you're looking for could not be found") ?>
+                <div class='jumbotron'>
+                    <p>
+                        <?php _e("The page you're looking for could not be found") ?>
 
-        <h2><?php _e('Pages') ?></h2>
-        <ul>
-            <?php wp_list_pages('title_li='); ?>
-        </ul>
+                    <h2><?php _e('Pages') ?></h2>
+                    <ul>
+                        <?php wp_list_pages('title_li='); ?>
+                    </ul>
 
-        <h2><?php _e('Pages by Categories') ?></h2>
-        <?php
-        $cats = get_categories();
-        foreach ($cats as $cat) {
-            query_posts('posts_per_page=-1&cat=' . $cat->cat_ID); ?>
-        <h3><?php echo $cat->cat_name; ?></h3>
-        <ul>
-            <?php while (have_posts()) {
-                    the_post();
-                ?>
-            <li><a href='<?php the_permalink(); ?>'><?php the_title(); ?></a></li>
-            <?php }
-                wp_reset_query(); ?>
-        </ul>
-        <?php } ?>
-        </p>
+                    <h2><?php _e('Pages by Categories') ?></h2>
+                    <?php
+					$cats = get_categories();
+					foreach ($cats as $cat) {
+						query_posts('posts_per_page=-1&cat=' . $cat->cat_ID); ?>
+                    <h3><?php echo $cat->cat_name; ?></h3>
+                    <ul>
+                        <?php while (have_posts()) {
+								the_post();
+							?>
+                        <li><a href='<?php the_permalink(); ?>'><?php the_title(); ?></a></li>
+                        <?php }
+							wp_reset_query(); ?>
+                    </ul>
+                    <?php } ?>
+                    </p>
+                </div>
+                <!-- /.jumbotron -->
+                <?php get_search_form(); ?>
+            </div>
+        </div>
+        <?php get_sidebar(); ?>
     </div>
-    <!-- /.jumbotron -->
-    <?php get_search_form(); ?>
-
 </div>
 <!-- /.container -->
 
